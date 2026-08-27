6b3cbdc06a3982478cda6dab44f6a16a86e48f9a
Hamzafarooq469 <hamzafarooq469@gmail.com>
2026-08-27 15:22:30 +0500

    feat(backend): set up server with Express, CORS, and database connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 
 const express = require("express")
+const dotenv = require("dotenv")
+const db = require("./config/db")
+const cors = require("cors")
+
+dotenv.config({
+    path: "./config/.env"
+})
+
 
 const app = express()
+db
 
-const PORT = 4000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+    res.send("Api is working")
+})
+
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
     console.log(`Server is working on port no: ${PORT}`)
